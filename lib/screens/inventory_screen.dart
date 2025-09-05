8c534a833aeec2b52008ce1a853b2b88aa78b52f
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2025-09-06 02:21:14 +0300

    added the intital selector screen for inventory --  products / categories

--- lib/screens/inventory_screen.dart
+++ lib/screens/inventory_screen.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:pixel_pos/screens/inventory_categories_screen.dart';
+import 'package:pixel_pos/screens/inventory_products_screen.dart';
+import 'package:pixel_pos/theme/app_theme.dart';
 
-class InventoryScreen extends StatelessWidget {
+class InventoryScreen extends StatefulWidget {
   const InventoryScreen({super.key});
 
   @override
+  State<InventoryScreen> createState() => _InventoryScreenState();
+}
+
+class _InventoryScreenState extends State<InventoryScreen> {
+  int _selectedIndex = 0;
+
+  // screens for products and categories
+  final List<Widget> _screens = [
+    InventoryCategoriesScreen(),
+    InventoryProductsScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("💼 Inventory", style: TextStyle(fontSize: 22)),
+    return Scaffold(
+      appBar: AppBar(title: const Text('Inventory')),
+      body: _screens[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        selectedItemColor: AppTheme.primaryColor,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: "Categories",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.inventory_2),
+            label: "Products",
+          ),
+        ],
+      ),
     );
   }
 }
