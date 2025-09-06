c22286b205e662208ffac656aea116af5af55314
yahya-nashar-FrequenC <dev2@frequenc.xyz>
2025-09-06 14:42:20 +0300

    added services code for sale-order and invoices

--- lib/data/database_invoice_service.dart
+++ lib/data/database_invoice_service.dart
@@ -1,1 +1,53 @@
-class DatabaseInvoiceService {}
+import 'package:pixel_pos/data/database_helper.dart';
+
+class DatabaseInvoiceService {
+  final DatabaseHelper _dbHelper = DatabaseHelper.instance;
+
+  // create invoice
+  Future<int> createInvoice(String name, String status, double total) async {
+    final db = await _dbHelper.database;
+    return await db.insert('invoices', {
+      'name': name,
+      'status': status,
+      'total': total,
+    });
+  }
+
+  // get all invoices
+  Future<List<Map<String, dynamic>>> getAllInvoices() async {
+    final db = await _dbHelper.database;
+    return await db.query('invoices');
+  }
+
+  // get by id
+  Future<Map<String, dynamic>?> getInvoiceById(int id) async {
+    final db = await _dbHelper.database;
+    final result = await db.query('invoices', where: 'id = ?', whereArgs: [id]);
+    if (result.isNotEmpty) {
+      return result.first;
+    }
+    return null;
+  }
+
+  // update invoice
+  Future<int> updateInvoice(
+    int id,
+    String name,
+    String status,
+    double total,
+  ) async {
+    final db = await _dbHelper.database;
+    return await db.update(
+      'invoices',
+      {'name': name, 'status': status, 'total': total},
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  // delete invoice
+  Future<int> deleteInvoice(int id) async {
+    final db = await _dbHelper.database;
+    return await db.delete('invoices', where: 'id = ?', whereArgs: [id]);
+  }
+}
--- lib/data/database_sale_order_service.dart
+++ lib/data/database_sale_order_service.dart
@@ -1,1 +1,51 @@
-class DatabaseSaleOrderService {}
+import 'package:pixel_pos/data/database_helper.dart';
+
+class DatabaseSaleOrderService {
+  final DatabaseHelper _dbHelper = DatabaseHelper.instance;
+
+  // create saleOrder
+  Future<int> createSaleOrder(int productId, int invoiceId) async {
+    final db = await _dbHelper.database;
+    return await db.insert('sale_orders', {
+      'product_id': productId,
+      'invoiceId': invoiceId,
+    });
+  }
+
+  // get all saleOrders
+  Future<List<Map<String, dynamic>>> getAllSaleOrders() async {
+    final db = await _dbHelper.database;
+    return await db.query('sale_orders');
+  }
+
+  // get by id
+  Future<Map<String, dynamic>?> getSaleOrderById(int id) async {
+    final db = await _dbHelper.database;
+    final result = await db.query(
+      'sale_orders',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+    if (result.isNotEmpty) {
+      return result.first;
+    }
+    return null;
+  }
+
+  // update saleOrder
+  Future<int> updateSaleOrder(int id, int productId, int invoiceId) async {
+    final db = await _dbHelper.database;
+    return await db.update(
+      'sale_orders',
+      {'product_id': productId, 'invoice_id': invoiceId},
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  // delete saleOrder
+  Future<int> deleteSaleOrder(int id) async {
+    final db = await _dbHelper.database;
+    return await db.delete('sale_orders', where: 'id = ?', whereArgs: [id]);
+  }
+}
